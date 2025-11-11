08e09b6ea5d0bea44a49836b9027f96611e4208d
Tomasz Durka <tomasz@durkapl>
2025-11-11 17:59:25 +0100

    chore: bump version to 1.13.1

--- src/generator/index.ts
+++ src/generator/index.ts
@@ -14,7 +14,7 @@ import {generateEnumFilters} from "./enum-filters-generator";
 generatorHandler({
   onManifest() {
     return {
-      version: '1.13.0',
+      version: '1.13.1',
       defaultOutput: 'src/generated',
       prettyName: 'NestJS API Generator',
     };
